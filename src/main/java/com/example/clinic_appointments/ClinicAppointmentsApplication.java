2c12c2fd231bfe11203b2d7412a8add15f7fb66d
antoniastanila <antoniamaria.stanila@gmail.com>
2025-12-04 15:12:02 +0200

    Initial Spring Boot project for clinic appointments

---
+++ src/main/java/com/example/clinic_appointments/ClinicAppointmentsApplication.java
@@ -0,0 +1,13 @@
+package com.example.clinic_appointments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicAppointmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicAppointmentsApplication.class, args);
+	}
+
+}
