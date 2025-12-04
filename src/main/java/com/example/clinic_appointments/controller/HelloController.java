9de5554c55201d969f97e2bb38002881559dfc9f
antoniastanila <antoniamaria.stanila@gmail.com>
2025-12-04 15:22:01 +0200

    added HelloController

---
+++ src/main/java/com/example/clinic_appointments/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.clinic_appointments.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Clinica pornita cu succes!";
+    }
+}
